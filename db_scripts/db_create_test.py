6b8181098b847936ae3c4370c1d08f49663ff254
MogliDerZauberKoenig <niko_faifert@web.de>
2025-02-04 17:00:18 +0100

    Create db_create_test.py
    Erstellt eine neue Tabelle für die Testdaten.

---
+++ db_scripts/db_create_test.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+connection = sqlite3.connect("../database.db")
+print(connection.total_changes)
+
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS temp (id INTEGER PRIMARY KEY NOT NULL, timestamp INTEGER, minValue REAL, maxValue REAL)")
+connection.close()
